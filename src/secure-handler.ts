83c9e5159b68a547968babb4acf9e26c62d6564b
Timo Benkhard <tcbenkhard@gmail.com>
2025-01-06 12:19:47 +0100

    Add event logging

--- src/secure-handler.ts
+++ src/secure-handler.ts
@@ -2,6 +2,7 @@ import {APIGatewayProxyEvent, Context} from "aws-lambda";
 import {wrapHandler} from "@tcbenkhard/aws-utils";
 
 const secure = async (event: APIGatewayProxyEvent, context: Context) => {
+    console.info(event)
     console.info(context)
     return `Authorized as ...`
 }
