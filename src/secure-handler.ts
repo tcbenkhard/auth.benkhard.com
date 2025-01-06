b4ddd91ba18a319954efcc45d47372423765645b
Timo Benkhard <tcbenkhard@gmail.com>
2025-01-06 12:24:12 +0100

    Return name of authorized user

--- src/secure-handler.ts
+++ src/secure-handler.ts
@@ -4,7 +4,7 @@ import {wrapHandler} from "@tcbenkhard/aws-utils";
 const secure = async (event: APIGatewayProxyEvent, context: Context) => {
     console.info(event)
     console.info(context)
-    return `Authorized as ...`
+    return `Authorized as ${event.requestContext.authorizer!.user}`
 }
 
 export const handler = wrapHandler(secure)
